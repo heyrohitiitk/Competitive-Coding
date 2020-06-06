77b6a35f48e7b7b48532ea7f1dc3dcfc118b11a1
Rohit kushwah <rohitk111099@gmail.com>
2020-06-06 17:06:30 +0530

    CPP-Template

---
+++ template.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+   //#include <boost/multiprecision/cpp_int.hpp>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+   //fo(i,n){fo(j,n) cout<<mat[i][j]<<"  ";cout<<"\n";}
+#define clr(x)  memset(x, 0, sizeof(x));
+#define si(x)  scanf("%d",&x)
+#define sl(x)  scanf("%lld",&x)
+#define ss(s)  scanf("%s",s)
+#define pi(x)  printf("%d\n",x)
+#define pl(x)  printf("%lld\n",x)
+#define ps(s)  printf("%s\n",s)
+#define deb(x) cout<<#x<<" = " <<x<<"\n"
+#define deb2(x,y) cout<<#x<<" = "<<x<<" , "<<#y<<" = "<<y<<"\n"
+#define wh(x) while(x--)
+#define all(x)  x.begin(),x.end()
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define pob pop_back
+#define pof pop_front
+#define MIN3(n1,n2,n3) min(n1,min(n2,n3))
+#define MIN4(n1,n2,n3,n4) min(n1,min(n2,min(n3,n4)))
+#define MAX3(n1,n2,n3) max(n1,max(n2,n3))
+#define MAX4(n1,n2,n3,n4) max(n1,max(n2,max(n3,n4)))
+#define armax(a,n) *max_element(a,a+n)
+#define armin(a,n) *min_element(a,a+n)
+#define arsum(a,n) accumulate(a,a+n,0)
+#define vesum(ve) accumulate(ve.begin(),ve.end(),0)
+#define vemax(ve) *max_element(ve.begin(),ve.end())
+#define vemin(ve) *min_element(ve.begin(),ve.end())
+#define vesort(ve) sort(ve.begin(),ve.end())
+#define vedsort(ve) sort(ve.begin(),ve.end(),greater<int>())
+#define boost                    \
+    ios::sync_with_stdio(false); \
+    cin.tie(0)
+#define fo(i,n) for(i=0;i<n;i++)
+#define foe(i,a,b) for(i=a;(a<b)?i<=b:i>=b;(a<b)?i++:i--)
+ll mod=1'000'000'007;
+ll inf=999'999'999'9;
+const int N=3e5;
+typedef pair<int,int>  pii;
+typedef pair<ll,ll>    pl;
+typedef vector<int>    vi;
+typedef vector<ll>     vl;
+typedef vector<pii>    vpi;
+typedef vector<pl>     vpl;
+typedef vector<vi>     vvi;
+typedef vector<vl>     vvl; 
+void solve(ll t)
+{
+   
+}
+int main()
+{
+   boost;
+   ll t=1;
+   cin>>t;
+
+   wh(t)
+     solve(t);
+
+   return 0;
+}
+
